c92be69d5cf6fe8992b53d17aee251f5c72ddf1f
nischal puri <mrnischalpuri@gmail.com>
2025-01-07 15:59:42 +0545

    [user create] in db using signup form

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,12 +1,42 @@
 "use server"
-
+import bcrypt from "bcrypt"
 import { SignupSchema, SignupSchemaType } from "@/schemas"
-
+import { db } from "@/lib/db"
 export const SignupAction = async (values: SignupSchemaType) => {
   const validationResuts = SignupSchema.safeParse(values)
   if (!validationResuts.success) {
     return { error: "Invalid inputs!" }
   }
 
-  return { success: "Register Successful!" }
+  // validation success
+  // extract validated data
+  const { email, password, name } = validationResuts.data
+
+  // check for email exit
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (existingUser) {
+    return { error: "Email already in use!" }
+  }
+
+  // hash the password
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  // save the user
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  // TODO: Send verification token to email
+
+  return { success: "User Created Successful!" }
 }
--- schemas/index.ts
+++ schemas/index.ts
@@ -14,8 +14,10 @@ export type LoginSchemaType = z.infer<typeof LoginSchema>
 // SIGN UP
 export const SignupSchema = z.object({
   email: z.string().email({ message: "Email is Required!" }),
-  password: z.string().min(1, { message: "Password is required!" }),
-  name: z.string().min(1, { message: "Name is required!" }),
+  password: z
+    .string()
+    .min(3, { message: "Password must be at least 3 characters long!" }),
+  name: z.string().min(6, { message: "Minimum 5 characters required!" }),
 })
 
 // Infer type for signup schema
