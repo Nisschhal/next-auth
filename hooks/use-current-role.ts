a4ca1c4b0982450ff0b5367cedda911bfd3fb75f
nischal puri <mrnischalpuri@gmail.com>
2025-01-11 15:05:57 +0545

    [userCurrent] hook updated to get fresh user each time

--- hooks/use-current-role.ts
+++ hooks/use-current-role.ts
@@ -1,16 +1,7 @@
 "use client"
 import { useSession } from "next-auth/react"
-import { useEffect } from "react"
 
 export const useCurrentRole = () => {
-  const { data, status, update } = useSession()
-
-  // Optionally refresh session data on component mount
-  useEffect(() => {
-    if (status === "authenticated") {
-      update()
-    }
-  }, [status, update])
-
-  return data?.user?.role
+  const session = useSession()
+  return session.data?.user.role
 }
