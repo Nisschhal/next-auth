d7a88f05d1b505ad9ef24d545d6df46c99334443
nischal puri <mrnischalpuri@gmail.com>
2025-01-11 13:22:43 +0545

    [modal] dialog added to sign in form

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,7 +1,13 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import LoginForm from "./login-form"
 interface LoginButtonProps {
   children: React.ReactNode
   mode?: "modal" | "redirect"
@@ -20,7 +26,15 @@ export const LoginButton = ({
   }
 
   if (mode === "modal") {
-    return <span>TODO: Implement modal</span>
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className="p-0 w-auto bg-transparent border-none">
+          <DialogTitle />
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    )
   }
 
   return (
