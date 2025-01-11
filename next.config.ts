dc49aad3ead3f3f8abc71de27b916b8add4d5d66
nischal puri <mrnischalpuri@gmail.com>
2025-01-11 14:26:05 +0545

    image pattern removed

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,20 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "avatars.githubusercontent.com" },
-      { protocol: "https", hostname: "utfs.io" },
-      {
-        protocol: "http",
-        hostname: "localhost:3000",
-        port: "",
-        pathname: "",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     { protocol: "https", hostname: "lh3.googleusercontent.com" },
+  //     { protocol: "https", hostname: "avatars.githubusercontent.com" },
+  //     { protocol: "https", hostname: "utfs.io" },
+  //     {
+  //       protocol: "http",
+  //       hostname: "localhost:3000",
+  //       port: "",
+  //       pathname: "",
+  //     },
+  //   ],
+  // },
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
   typescript: {
     ignoreBuildErrors: true,
   },
